c6526e50aeb724239d46e20ba815cc2ea4eac0ee
Venice Vargas <venicevargas1@live.com>
2020-09-07 23:06:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,4 +13,15 @@ class Person
 end 
 
 class Person
+    def initialize(job)
+      @job=job
+    end
+    
     def job
+      @job
+    end
+    
+    def job=(new_job)
+      @job=new_job
+    end
+  end
