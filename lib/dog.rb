dfede87acf2ca98abd04db79168ecc08e9e02707
Venice Vargas <venicevargas1@live.com>
2020-09-07 23:09:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog 
+  def initialize=(dogs_name)
+    @name=dogs_name 
+  end
+  
+  def name
+    @name
+  end 
+  
+  def name=(new_name)
+    @name=new_name
+  end
+end
+
+class Dog 
+  def initialize=(dog_breed)
+    @breed=dog_breed
+  end
+  
+  def breed 
+    @breed
+  end 
+  
+  def breed=(new_breed)
+    @breed=new_breed
+  end
+end 
